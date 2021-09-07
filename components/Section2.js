7ddeabebb3c8aa12f1586889d927598506181be4
Raulito-Moya <rauly7moya@gmail.com>
2021-09-06 21:51:30 -0500

    creating production build

--- components/Section2.js
+++ components/Section2.js
@@ -1,4 +1,5 @@
 import styles from '../styles/Home.module.css'
+import Image from 'next/image'
 
 export default function Section2(){
 
@@ -7,12 +8,12 @@ export default function Section2(){
   <section className={styles.sub_section} id="about_me">
     <div className={styles.information}>
        <h2>About Me</h2>
-       <p>Hello my name is Raul Moya. I'm a software developer and I would like help you to build great things ....Cuando utilizas el margen y el área de relleno en porcentajes, el valor se calcula a partir del tamaño en línea del elemento, es decir, el ancho del elemento cuando se trabaja en un lenguaje horizontal. En nuestro ejemplo, todos los márgenes y el área de relleno son del 10% del ancho del elemento. Esto significa que puedes tener márgenes y relleno alrededor de la caja del mismo tamaño. Este es un hecho que vale la pena recordar si utilizas porcentajes de esta manera.</p>
+       <p>Hello my name is Raul Moya. I m a software developer and I would like help you to build great things ....Cuando utilizas el margen y el área de relleno en porcentajes, el valor se calcula a partir del tamaño en línea del elemento, es decir, el ancho del elemento cuando se trabaja en un lenguaje horizontal. En nuestro ejemplo, todos los márgenes y el área de relleno son del 10% del ancho del elemento. Esto significa que puedes tener márgenes y relleno alrededor de la caja del mismo tamaño. Este es un hecho que vale la pena recordar si utilizas porcentajes de esta manera.</p>
     </div>  
     <div className={styles.headshot_container}> 
-       <img className={styles.headshot} src="/headphoto.jpg" alt="Raul Moya Photo"/>
-       <img className={styles.headshot} src="/headphoto1.jpg" alt=""/>
-       <img className={styles.headshot} src="/headphoto2.jpg" alt=""/>
+       <img className={styles.headshot} src="/headphoto1.jpg"  alt="Raul Moya Photo"/>
+       <img className={styles.headshot} src="/headphoto.jpg"   alt="Raul Moya Photo"/>
+       <img className={styles.headshot} src="/headphoto2.jpg"  alt="Raul Moya Photo"/>
     </div>
       
     <div className={styles.skillscontainer}>
@@ -20,14 +21,14 @@ export default function Section2(){
   
       <div className={styles.skills}>
          
-          <img className={styles.skill_icon} src="/skills/html5.svg" alt=""/>
-          <img className={styles.skill_icon} src="/skills/css3.svg" alt=""/>
-          <img className={styles.skill_icon} src="/skills/javascript.svg" alt=""/>
-          <img className={styles.skill_icon} src="/skills/github.svg" alt=""/>
-          <img className={styles.skill_icon} src="/skills/react.svg" alt=""/>
-          <img className={styles.skill_icon} src="/skills/nodejs.svg" alt=""/>
-          <img className={styles.skill_icon} src="/skills/mongodb.svg" alt=""/>
-          <img className={styles.skill_icon} src="/skills/mysql.svg" alt=""/>
+          <Image className={styles.skill_icon} width={50} height={50} src="/skills/html5.svg" alt=""/>
+          <Image className={styles.skill_icon} width={50} height={50} src="/skills/css3.svg" alt=""/>
+          <Image className={styles.skill_icon} width={50} height={50} src="/skills/javascript.svg" alt=""/>
+          <Image className={styles.skill_icon} width={50} height={50} src="/skills/github.svg" alt=""/>
+          <Image className={styles.skill_icon} width={50} height={50} src="/skills/react.svg" alt=""/>
+          <Image className={styles.skill_icon} width={50} height={50} src="/skills/nodejs.svg" alt=""/>
+          <Image className={styles.skill_icon} width={50} height={50} src="/skills/mongodb.svg" alt=""/>
+          <Image className={styles.skill_icon} width={50} height={50} src="/skills/mysql.svg" alt=""/>
       </div>
     </div>
   </section>
--- components/Section3.js
+++ components/Section3.js
@@ -1,7 +1,8 @@
 import { useRef, useState } from 'react'
+import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import ComponentShow from './ComponentShow'
-import Modal from './Modal'
+import Modal from '../pages/Modal'
 
 
 export default function Section3(){
@@ -22,36 +23,36 @@ export default function Section3(){
     <section className={styles.sub_section_alternative} id="projects">
       <h2 className={styles.project_title}>Projects</h2>  
       <div className={styles.project_container}>
-        <div ref={project1} className={styles.project_card} onClick={()=> {setElement(project1)  ; setShowModal(true)} }>
-           <img className={styles.project_image} src="/project-image.png" alt=""/>
+        <div ref={project1} className={styles.project_card}  onClick={()=> {setElement(project1)  ; setShowModal(true)} }>
+           <Image className={styles.project_image}  src="/project-image.png" width={200} height={100}  layout="responsive"  alt=""/>
            <h3 className={styles.project_title}>Healty Habits</h3>
            <p className={styles.subtext} >This a community blog about nutrition habits, where you can become a member and upload your articles and posts on specific topics of healthy habits </p>
            <hr/>
            <p className={styles.link}>View Here</p>
         </div>
         <div ref={project2} className={styles.project_card}   onClick={()=> {setElement(project2)  ; setShowModal(true)} }>
-           <img className={styles.project_image} src="" alt=""/>
+           <Image className={styles.project_image} width={200} height={100}  layout="responsive"  src="/project-image.png" alt=""/>
             <h3 className={styles.project_title}>Project Two</h3>
-           <p className={styles.subtext}>This it's a Calendar App  where you can become a member and register events with descriptions at a specific time in your calendar </p>
+           <p className={styles.subtext}>This its a Calendar App  where you can become a member and register events with descriptions at a specific time in your calendar </p>
            <hr/>
            <p className={styles.link}>View Here</p>
         </div>
         <div ref={project3} className={styles.project_card}   onClick={()=> {setElement(project3)  ; setShowModal(true)} }>
-          <img className={styles.project_image} src="" alt=""/>
+          <Image className={styles.project_image} width={200} height={100}  layout="responsive"  src="/project-image.png" alt=""/>
             <h3>Project Three</h3>
            <p className={styles.subtext}>If you like Marvel or DC comics, you will surely like this application, with this simple application you can see a series of cards about Marvel and DC characters and see information about them </p>
            <hr/>
            <p className={styles.link}>View Here</p>
         </div>
         <div ref={project4} className={styles.project_card}   onClick={()=> {setElement(project4)  ; setShowModal(true)} }>
-           <img className={styles.project_image} src="" alt=""/>
+           <Image className={styles.project_image} width={200} height={100}  layout="responsive"  src="/project-image.png" alt=""/>
              <h3 className={styles.project_title}>Project fourth</h3>
-           <p className={styles.subtext}>maybe you don't want to keep your notes in solid right? And that's why I present you this JournalApp application, here you can become a member and add your notes with images and description included, I hope you enjoy it </p>
+           <p className={styles.subtext}>maybe you don t want to keep your notes in solid right? And thats why I present you this JournalApp application, here you can become a member and add your notes with images and description included, I hope you enjoy it </p>
            <hr/>
            <p className={styles.link}>View Here</p>
         </div>
         <div ref={project5} className={styles.project_card}   onClick={()=> {setElement(project5)  ; setShowModal(true)} }>
-           <img className={styles.project_image} src="" alt=""/>
+           <Image className={styles.project_image} width={200} height={100}  layout="responsive"  src="/project-image.png" alt=""/>
              <h3 className={styles.project_title}>Project Fight</h3>
            <p className={styles.subtext}>If you like music why not keep an eye on Spotify, in this application you can search for the most recent albums of your favorite artists and click on the link and go directly to the spotify platform and listen to them, enjoy it </p>
            <hr/>

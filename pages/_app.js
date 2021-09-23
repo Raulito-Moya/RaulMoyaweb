6df36b7ef8ec7531d7f924204f159568a40392af
Raulito-Moya <rauly7moya@gmail.com>
2021-09-22 23:05:54 -0500

    implementing analitics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,47 @@
 import '../styles/globals.css'
+import Script from 'next/script'
+
+// Global site tag (gtag.js) - Google Analytics 
+//<script async src="https://www.googletagmanager.com/gtag/js?id=G-FK7XZ2NHH5"></script>
+//<script>
+//  window.dataLayer = window.dataLayer || [];
+//  function gtag(){dataLayer.push(arguments);}
+//  gtag('js', new Date());
+//
+//  gtag('config', 'G-FK7XZ2NHH5');
+//</script>
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+   
+  const code = process.env.ANALYTICS
+  
+  return ( 
+   <>
+  <Script
+   id="my-script"
+   strategy='lazyOnload'
+   src={`https://www.googletagmanager.com/gtag/js?id=${code}`}
+  />
+  <Script
+  id="my-script"
+  strategy='lazyOnload' >
+    {
+      `
+     window.dataLayer = window.dataLayer || [];
+     function gtag(){dataLayer.push(arguments);}
+     gtag('js', new Date());
+
+     gtag('config', ${code});
+     ` 
+    }
+ 
+    
+  </Script>
+   
+  <Component {...pageProps} />
+  </>
+  )
 }
 
 export default MyApp
